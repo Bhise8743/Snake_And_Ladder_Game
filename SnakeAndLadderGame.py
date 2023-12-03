487cd88410970553d15364d2dfe8ced851346a4b
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-12-03 11:48:04 +0530

    [Omkar] Add : Program to play the snake lder game of one player star position is 0

---
+++ SnakeAndLadderGame.py
@@ -0,0 +1,19 @@
+"""
+
+@Author: Omkar Bhise
+
+@Date: 2023-12-02 05:00:30
+
+@Last Modified by: Omkar Bhise
+
+@Last Modified time: 2023-12-03 5:30:00
+
+@Title :  Snake and ladder game
+
+"""
+import random
+
+player_pos = 0  # global variable
+
+if __name__ == '__main__':  # UC1
+    print(f"Snake and Ladder game played with single player at start position {player_pos} ")
