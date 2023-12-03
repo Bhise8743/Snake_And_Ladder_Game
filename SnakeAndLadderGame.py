1ab13a17a555c4517d3b7f0789f1203328ee2e43
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-12-03 11:54:24 +0530

    [Omkar] Add : Program to die get a number between 1 to 6

--- SnakeAndLadderGame.py
+++ SnakeAndLadderGame.py
@@ -6,14 +6,27 @@
 
 @Last Modified by: Omkar Bhise
 
-@Last Modified time: 2023-12-03 5:30:00
+@Last Modified time: 2023-12-01 5:30:00
 
 @Title :  Snake and ladder game
 
 """
 import random
 
-player_pos = 0  # global variable
+player_pos = 0
+
+
+def die():  # UC2
+    """
+        Description:
+            it returns the random number between one and six
+        Parameter:
+            None
+
+        Return: int value
+    """
+    return random.randint(1, 6)
+
 
 if __name__ == '__main__':  # UC1
     print(f"Snake and Ladder game played with single player at start position {player_pos} ")
